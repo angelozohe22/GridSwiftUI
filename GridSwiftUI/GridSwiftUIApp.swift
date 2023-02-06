33d8ec32a20216f28d115577fd77f9ed8d16e1e3
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-06 11:16:29 -0500

    Initial Commit

---
+++ GridSwiftUI/GridSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSwiftUIApp.swift
+//  GridSwiftUI
+//
+//  Created by Luis Angel Inga Mendoza on 6/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GridSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
